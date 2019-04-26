2a8699484e2f834929be2c92112f3052dd63c581
ndbSon <49903834+ndbSon@users.noreply.github.com>
2019-04-26 23:22:38 +0700

    JS
    add_JS

---
+++ js/BM.js
@@ -0,0 +1,22 @@
+
+$(document).ready(function ($) {
+
+  //newstiecker
+  $('#newsTicker1').breakingNews()
+  //scroll
+  $('.scroll').click(function () {
+    $('html, body').animate({
+      scrollTop: $('[name="' + $.attr(this, 'href').substr(1) + '"]').offset().top
+    }, 10000);
+
+    return false;
+  });
+  initComparisons();
+
+  //video-----------
+
+ 
+
+
+
+});
